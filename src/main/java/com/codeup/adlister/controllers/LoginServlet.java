4b30e80720420c71e1e990ac35e837727003340e
Yamba Ouandaogo <yamba.ouandaogo@outlook.com>
2023-05-18 14:43:07 -0500

    adding navbar changes based on log status

--- src/main/java/com/codeup/adlister/controllers/LoginServlet.java
+++ src/main/java/com/codeup/adlister/controllers/LoginServlet.java
@@ -35,6 +35,7 @@ public class LoginServlet extends HttpServlet {
         boolean validAttempt = Password.check(password, user.getPassword());
 
         if (validAttempt) {
+
             request.getSession().setAttribute("user", user);
             response.sendRedirect("/profile");
         } else {
--- src/main/webapp/WEB-INF/login.jsp
+++ src/main/webapp/WEB-INF/login.jsp
@@ -7,6 +7,7 @@
 </head>
 <body>
     <jsp:include page="/WEB-INF/partials/navbar.jsp" />
+
     <div class="container">
         <h1>Please Log In</h1>
         <form action="/login" method="POST">
