ea4561e287d9328c84f30c1ec96c47830e9bbbd7
Gabriella Martinez <gabriellamartinez0819@gmail.com>
2023-05-18 10:10:29 -0500

    configured local Config file

--- src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
@@ -2,6 +2,7 @@ package com.codeup.adlister.dao;
 
 import com.codeup.adlister.models.User;
 import com.mysql.cj.jdbc.Driver;
+import config.Config;
 
 import java.sql.*;
 
@@ -20,7 +21,6 @@ public class MySQLUsersDao implements Users {
             throw new RuntimeException("Error connecting to the database!", e);
         }
     }
-
 
     @Override
     public User findByUsername(String username) {
