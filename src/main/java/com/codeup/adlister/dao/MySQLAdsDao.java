b4eafd6aa77cdcc8875b89f2fc0ae2e782f48f60
Gabriella Martinez <gabriellamartinez0819@gmail.com>
2023-05-19 13:35:18 -0500

    added category logic to the search bar

--- src/main/java/com/codeup/adlister/dao/MySQLAdsDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLAdsDao.java
@@ -70,9 +70,10 @@ public class MySQLAdsDao implements Ads {
     public List<Ad> searchAds(String search) {
         try {
             // refactor to where it searches the title and description
-            String insertQuery = "SELECT * FROM ads WHERE title LIKE ?";
+            String insertQuery = "SELECT * FROM ads WHERE title LIKE ? OR category LIKE ?";
             PreparedStatement stmt = connection.prepareStatement(insertQuery, Statement.RETURN_GENERATED_KEYS);
             stmt.setString(1, "%" + search + "%");
+            stmt.setString(2, "%" + search + "%");
             ResultSet resultSet = stmt.executeQuery();
             return createAdsFromResults(resultSet);
         } catch (SQLException e) {
